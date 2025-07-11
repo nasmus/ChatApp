3bb2d7a510b33ac126290d0bf9e8ae94a37b632d
nasmus <nasmus.shahadat@gmail.com>
2025-07-11 11:09:33 +0600

    login Update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
     @user = current_user
     @users = User.where.not(id: current_user.id)
-    @groups = current_user.chat_rooms.group(:name).includes(:users)
+    @groups = current_user.chat_rooms.where(chat_type: "group_chat").includes(:users)
+
   end
 end
